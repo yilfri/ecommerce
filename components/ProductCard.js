faa5c428ebf026357ef40978cae8b568d6ace7b1
yilfri <yilfri97@hotmail.com>
2021-07-21 21:48:33 -0400

    Refactorizacion de componentes para dejar los productos separados del index.

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,25 +1,75 @@
-import React, { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import CartContext from '../context/cart/cartContext';
+import Image from 'next/image';
+
+// Prime React Dependencies
+import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+// Styled Components.
+import { CardFlex, ProductPrice, ProductContainer } from '../components/styles/home';
 
 const ProductCard = ({ product }) => {
 	// Destructuración de props.
-	const { name, price, quantity, description, id, img } = product;
+	const { name, quantity, price, description, img } = product;
 
 	// Obtener datos del Context.
 	const { decreaseProductQty, addProductCart } = useContext(CartContext);
 
+	// Creación de Ref.
+	const toast = useRef(null);
+
+	// Handle event - OnClick.
 	const handleAddToCart = (product) => {
 		decreaseProductQty(product);
 		addProductCart(product);
-	};
-	return (
-		<>
-			<li>
-				Product: {name} - Price: {price} - Cantidad: {quantity}
-			</li>
 
-			{quantity !== 0 && <button onClick={() => handleAddToCart(product)}>Add cart</button>}
-		</>
+		toast.current.show({
+			severity: 'success',
+			summary: 'Producto agregado',
+			detail: `Haz agreagado un ${product.name} al carrito`,
+			life: 3000
+		});
+	};
+
+	return (
+		<div className="p-col-12 p-md-4 ">
+			<CardFlex className="card">
+				<div>
+					<Image
+						src={img.front}
+						onError={(e) =>
+							(e.target.src =
+								'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png')
+						}
+						alt={name}
+						width={300}
+						height={300}
+						layout="responsive"
+					/>
+					<h3>{name}</h3>
+					<p>{description}</p>
+					<span>
+						Pzas disponibles: <b>{quantity} </b>
+					</span>
+				</div>
+				<div className="p-d-flex p-jc-between p-ai-center p-mt-4">
+					<ProductPrice>${price}</ProductPrice>
+					<Button
+						icon="pi pi-shopping-cart"
+						label="Agregar"
+						disabled={quantity === 0}
+						onClick={() => handleAddToCart(product)}
+					></Button>
+				</div>
+			</CardFlex>
+
+			<Toast ref={toast} position="bottom-right" />
+		</div>
 	);
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,14 @@
-import { useEffect, useContext, useRef } from 'react';
+import { useEffect, useContext } from 'react';
 import CartContext from '../context/cart/cartContext';
 import Layout from '../components/layout/Layout';
-import Image from 'next/image';
+import ProductCard from '../components/ProductCard';
 
 // Prime React Dependencies
-import { Button } from 'primereact/button';
 import { DataView } from 'primereact/dataview';
-import { Toast } from 'primereact/toast';
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
 
 // Styled Components.
-import { CardFlex, ProductPrice, ProductContainer } from '../components/styles/home';
+import { ProductContainer } from '../components/styles/home';
 
 export default function Home() {
 	// Destructuración de datos del context
@@ -24,69 +19,22 @@ export default function Home() {
 		getProducts();
 	}, []);
 
-	const toast = useRef(null);
-
-	// Handle Events.
-	const handleAddToCart = (product) => {
-		decreaseProductQty(product);
-		addProductCart(product);
-
-		toast.current.show({
-			severity: 'success',
-			summary: 'Producto agregado',
-			detail: `Haz agreagado un ${product.name} al carrito`,
-			life: 3000
-		});
-	};
-
 	const itemTemplate = (product) => {
 		if (!product) return;
 
-		return renderGridItem(product);
-	};
-
-	const renderGridItem = (data) => {
-		return (
-			<div className="p-col-12 p-md-4 ">
-				<CardFlex className="card">
-					<div>
-						<Image
-							src={data.img.front}
-							onError={(e) =>
-								(e.target.src =
-									'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png')
-							}
-							alt={data.name}
-							width={300}
-							height={300}
-							layout="responsive"
-						/>
-						<h3>{data.name}</h3>
-						<p>{data.description}</p>
-						<span>
-							Pzas disponibles: <b>{data.quantity} </b>
-						</span>
-					</div>
-					<div className="p-d-flex p-jc-between p-ai-center p-mt-4">
-						<ProductPrice>${data.price}</ProductPrice>
-						<Button
-							icon="pi pi-shopping-cart"
-							label="Add to Cart"
-							disabled={data.quantity === 0}
-							onClick={() => handleAddToCart(data)}
-						></Button>
-					</div>
-				</CardFlex>
-			</div>
-		);
+		return <ProductCard product={product} />;
 	};
 
 	return (
 		<Layout title="FunKommerce" description="Tienda Online de Funko Pop">
-			<ProductContainer className="card verdecito">
-				<Toast ref={toast} position="bottom-right" />
-
-				<DataView value={products} itemTemplate={itemTemplate} paginator rows={9} />
+			<ProductContainer>
+				<DataView
+					value={products}
+					itemTemplate={itemTemplate}
+					emptyMessage={'No hay productos disponibles'}
+					paginator
+					rows={9}
+				/>
 			</ProductContainer>
 		</Layout>
 	);

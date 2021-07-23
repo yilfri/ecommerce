592cf5f8104d123ab2973c74dad49ccdb534d938
yilfri <yilfri97@hotmail.com>
2021-07-23 10:46:30 -0400

    Clases agregadas

--- components/styles/cartStyles.js
+++ components/styles/cartStyles.js
@@ -1,24 +1,41 @@
 import styled from 'styled-components';
 
-export const UlProducts = styled.ul`
-	margin: 0 1rem;
-	padding: 0;
-	border: 1px solid #dee2e6;
-`;
-
 export const Title = styled.h1`
 	text-align: center;
+`;
+
+export const wrapper = styled.h1`
+	text-align: center;
+`;
+
+export const UlProducts = styled.ul`
+	margin-bottom: 2rem;
+	padding: 0;
+	border: 1px solid #dee2e6;
 `;
 
 export const NoItems = styled.p`
 	text-align: center;
 `;
 
+export const SubTotal = styled.section`
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-end;
+	align-items: center;
+	padding: 0.5rem;
+	width: 100%;
+
+	span {
+		font-weight: bold;
+		margin-left: 1rem;
+	}
+`;
+
 export const TotalAmount = styled.div`
-	text-align: right;
-	padding: 1rem;
-	margin: 1rem;
-	background-color: #f5f5f5;
+	/* text-align: left;
+	padding: 0 2rem;
+	background-color: #e1e1e1;
 
 	h4 {
 		font-size: 1.5rem;
@@ -27,11 +44,11 @@ export const TotalAmount = styled.div`
 	}
 
 	p {
-		font-weight: bold;
 		line-height: 0.5rem;
+		padding: 0 1rem;
 	}
 
 	span {
 		font-weight: normal;
-	}
+	} */
 `;
